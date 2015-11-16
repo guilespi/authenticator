32a5b30b62da9989b90c482cfb235fe48e85a1fa
Guillermo Winkler <guillermo.winkler@gmail.com>
2015-11-16 11:45:13 -0300

    Authenticator init commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject afis.authenticator "0.1.0"
+  :description "Authenticator Component"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.intception.components/lifecycle "0.1.6"]
+                 [com.taoensso/carmine "2.7.0"]])
---
+++ src/authenticator/service.clj
@@ -0,0 +1,69 @@
+(ns authenticator.service
+  (:require [taoensso.carmine :as car])
+  (:use components.lifecycle.protocol))
+
+
+(defmacro exec
+  [service & body]
+  `(car/wcar (handler ~service) ~@body))
+
+
+(defn set-and-expire!
+  [redis redis-ns id value ttl]
+  (exec redis
+        (car/set (str redis-ns ":" id) value)
+        (car/expire (str redis-ns ":" id) ttl)))
+
+
+(defn get-key
+  [redis redis-ns id]
+  (exec redis
+        (car/get (str redis-ns ":" id))))
+
+(defn del!
+  [redis redis-ns id]
+  (exec redis (car/del (str redis-ns ":" id))))
+
+(def auth-ns "Authenticator")
+
+(defprotocol Authenticator
+  (generate-token [this id] "Creates a temporary token given an identifier")
+  (resolve-token [this token] "Given a token returns related identifier"))
+
+
+(defrecord AuthComponent
+    [state settings]
+
+  Lifecycle
+  (stop [this system]
+    )
+
+  (start [this system]
+    (swap! state
+           assoc :redis
+           {:pool {} :spec {:host (:host settings)
+                            :port (:port settings)}}))
+
+  Service
+  (handler [_]
+    (:redis @state))
+
+  Authenticator
+  (generate-token [this id]
+    (let [new-token (str (java.util.UUID/randomUUID))]
+      (set-and-expire! this
+                       auth-ns
+                       new-token
+                       (str id)
+                       (or (:expire-timeout settings) 30))
+      new-token))
+
+  (resolve-token [this token]
+    (when-let [id (get-key this auth-ns token)]
+      (del! this auth-ns token)
+      id)))
+
+(defn make
+  "Creates an authenticator component"
+  [settings]
+  (->AuthComponent (atom {}) settings))
